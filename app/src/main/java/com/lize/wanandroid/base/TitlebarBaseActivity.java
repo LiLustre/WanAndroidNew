1b3562e0b76865dafdaf603806c88443af720329
lize <lize@zhumengyuan.com>
2019-10-12 17:33:00 +0800

    <添加标题栏Activity>

---
+++ app/src/main/java/com/lize/wanandroid/base/TitlebarBaseActivity.java
@@ -0,0 +1,18 @@
+package com.lize.wanandroid.base;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+/**
+ * @author Lize
+ * on 2019/10/12
+ */
+public class TitlebarBaseActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
