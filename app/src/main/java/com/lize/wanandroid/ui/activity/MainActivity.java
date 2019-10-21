024802a2e0dcc42f0b5c83e40fbd681c858b00cb
lize <lize@zhumengyuan.com>
2019-10-21 14:57:36 +0800

    <替换底部导航栏>

--- app/src/main/java/com/lize/wanandroid/ui/activity/MainActivity.java
+++ app/src/main/java/com/lize/wanandroid/ui/activity/MainActivity.java
@@ -2,38 +2,37 @@ package com.lize.wanandroid.ui.activity;
 
 
 import android.os.Bundle;
-
-import android.view.MenuItem;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.bottomnavigation.LabelVisibilityMode;
 import com.lize.wanandroid.R;
 import com.lize.wanandroid.base.activity.BaseActivity;
 import com.lize.wanandroid.databinding.ActivityMainBinding;
 import com.lize.wanandroid.ui.fragment.ClassifyFragment;
-import com.lize.wanandroid.ui.fragment.index.IndexFragment;
 import com.lize.wanandroid.ui.fragment.MeFragment;
 import com.lize.wanandroid.ui.fragment.PiazzaFragment;
-import com.lize.wanandroid.ui.helper.BottomNavigationViewHelper;
-
-import static com.google.android.material.bottomnavigation.LabelVisibilityMode.LABEL_VISIBILITY_LABELED;
+import com.lize.wanandroid.ui.fragment.index.IndexFragment;
 
 public class MainActivity extends BaseActivity<ActivityMainBinding> {
-    private static final String SELECT_ITEM = "bottomNavigationSelectItem";
+    private static final String SELECT_ITEM_POS = "bottomNavigationSelectItem";
+    private static final int HOME_SELECT_ITEM_POS = 0;
+    private static final int CLASSIFY_SELECT_ITEM_POS = 1;
+    private static final int SQUARESELECT_ITEM_POS = 2;
+    private static final int ME_SELECT_ITEM_POS = 3;
     private ClassifyFragment classifyFragment;
     private IndexFragment indexFragment;
     private PiazzaFragment piazzaFragment;
     private MeFragment meFragment;
-    private int selectedBottomId;
+    private int selectedBottomPos;
 
     @Override
     public int getLayoutId() {
         return R.layout.activity_main;
     }
+
     @Override
     protected void initView(@Nullable Bundle savedInstanceState) {
 
@@ -43,43 +42,65 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
             piazzaFragment = (PiazzaFragment) getSupportFragmentManager().findFragmentByTag(PiazzaFragment.class.getName());
             meFragment = (MeFragment) getSupportFragmentManager().findFragmentByTag(MeFragment.class.getName());
             // 恢复 recreate 前的位置
-            if (savedInstanceState.getInt(SELECT_ITEM) == 0) {
-                binding.bnvBar.setSelectedItemId(R.id.menu_item_index);
-                showFragment(R.id.menu_item_index);
-            } else {
-                binding.bnvBar.setSelectedItemId(savedInstanceState.getInt(SELECT_ITEM));
-                showFragment(savedInstanceState.getInt(SELECT_ITEM));
-            }
+            showFragment(savedInstanceState.getInt(SELECT_ITEM_POS));
         } else {
-            showFragment(R.id.menu_item_index);
+            showFragment(HOME_SELECT_ITEM_POS);
         }
-        initBottomTab();
+        // initBottomTab();
     }
 
-    private void initBottomTab() {
-       // BottomNavigationViewHelper.disableShiftMode(binding.bnvBar);
-        binding.bnvBar.setLabelVisibilityMode(LABEL_VISIBILITY_LABELED);
-        binding.bnvBar.setOnNavigationItemSelectedListener(new BottomNavigationView
-                .OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                showFragment(item.getItemId());
-                return true;
-            }
-        });
+    public void onHomeClick(View view) {
+        showFragment(HOME_SELECT_ITEM_POS);
     }
 
-    private void showFragment(@NonNull int itemId) {
+    public void onClassifyClick(View view) {
+        showFragment(CLASSIFY_SELECT_ITEM_POS);
+    }
+
+    public void onSquareClick(View view) {
+        showFragment(SQUARESELECT_ITEM_POS);
+    }
+
+    public void onMeClick(View view) {
+        showFragment(ME_SELECT_ITEM_POS);
+    }
+
+
+    private void checkHomeBtn() {
+        binding.homeRb.setSelected(true);
+        binding.meRb.setSelected(false);
+        binding.classifyRb.setSelected(false);
+        binding.squareRb.setSelected(false);
+    }
+
+    private void checkClassifyBtn() {
+        binding.homeRb.setSelected(false);
+        binding.classifyRb.setSelected(true);
+        binding.squareRb.setSelected(false);
+        binding.meRb.setSelected(false);
+    }
+
+    private void checkSquareBtn() {
+        binding.homeRb.setSelected(false);
+        binding.meRb.setSelected(false);
+        binding.classifyRb.setSelected(false);
+        binding.squareRb.setSelected(true);
+    }
+
+    private void checkMeBtn() {
+        binding.homeRb.setSelected(false);
+        binding.meRb.setSelected(true);
+        binding.classifyRb.setSelected(false);
+        binding.squareRb.setSelected(false);
+    }
+
+
+    private void showFragment(@NonNull int itemPos) {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         hideFragment(ft);
-
-        if (itemId == 0) {
-            selectedBottomId = R.id.menu_item_index;
-        } else {
-            selectedBottomId = itemId;
-        }
-        switch (selectedBottomId) {
-            case R.id.menu_item_index:
+        selectedBottomPos = itemPos;
+        switch (selectedBottomPos) {
+            case HOME_SELECT_ITEM_POS:
                 /**
                  * 如果Fragment为空，就新建一个实例
                  * 如果不为空，就将它从栈中显示出来
@@ -90,24 +111,27 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
                 } else {
                     ft.show(indexFragment);
                 }
+                checkHomeBtn();
                 break;
-            case R.id.menu_item_classify:
+            case CLASSIFY_SELECT_ITEM_POS:
                 if (classifyFragment == null) {
                     classifyFragment = ClassifyFragment.getInstance();
                     ft.add(R.id.content_fl, classifyFragment, ClassifyFragment.class.getName());
                 } else {
                     ft.show(classifyFragment);
                 }
+                checkClassifyBtn();
                 break;
-            case R.id.menu_item_piazza:
+            case SQUARESELECT_ITEM_POS:
                 if (piazzaFragment == null) {
                     piazzaFragment = PiazzaFragment.getInstance();
                     ft.add(R.id.content_fl, piazzaFragment, PiazzaFragment.class.getName());
                 } else {
                     ft.show(piazzaFragment);
                 }
+                checkSquareBtn();
                 break;
-            case R.id.menu_item_me:
+            case ME_SELECT_ITEM_POS:
                 if (meFragment == null) {
 
                     meFragment = MeFragment.getInstance();
@@ -115,18 +139,18 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
                 } else {
                     ft.show(meFragment);
                 }
+                checkMeBtn();
                 break;
         }
         ft.commit();
     }
 
 
-
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         // recreate 时记录当前位置 (在 Manifest 已禁止 Activity 旋转,所以旋转屏幕并不会执行以下代码)
         super.onSaveInstanceState(outState);
-        outState.putInt(SELECT_ITEM, binding.bnvBar.getSelectedItemId());
+        outState.putInt(SELECT_ITEM_POS, selectedBottomPos);
     }
 
     private void hideFragment(FragmentTransaction ft) {
